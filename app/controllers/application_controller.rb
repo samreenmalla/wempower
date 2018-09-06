6427ddc0b7ffa2df09b09e37f0947d91505fbca4
swojeet <swojeetkayastha@gmail.com>
2018-09-06 20:32:23 +0545

    Permit additional params for users during sign up and update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
   #protect CSRF attacks by raising exceptions
   protect_from_forgery with: :exception
 
-   before_action :authenticate_user! 
+   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
 	def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :photo) }
+  	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :firstname, :lastname, :roll_number, :batch, :faculty, :phone_number, :address, :invitation_token, :photo) }
   	devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-  	devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name, :photo) }
+  	devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :firstname, :lastname, :roll_number, :batch, :faculty, :phone_number, :address, :invitation_token, :photo) }
 	end
 end
